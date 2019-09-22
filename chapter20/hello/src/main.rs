97264273572d725511fccca7606b776a86fd92b0
Sammy <lixiangmin01@baidu.com>
2019-09-22 12:00:04 +0800

    Listing 20-5: Sending the contents of hello.html as the body of the response

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -17,7 +18,9 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
