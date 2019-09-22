40d768ce925036fe37cf3a95883686c4258bb9ea
Sammy <lixiangmin01@baidu.com>
2019-09-22 12:33:42 +0800

    feature(chapter20): Listing 20-21: Adding a new trait FnBox to work around the current limitations of Box<FnOnce()>

--- chapter20/hello/src/lib.rs
+++ chapter20/hello/src/lib.rs
@@ -8,7 +8,17 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-type Job = Box<FnOnce() + Send + 'static>;
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<FnBox + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -50,12 +60,12 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| loop {
+        let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
 
             println!("Worker {} got a job; executing.", id);
 
-            (*job)();
+            job.call_box();
         });
 
         Worker { id, thread }
