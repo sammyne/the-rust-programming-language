6c1504ff2e24280e5664a7c1cea4798eb131d113
Sammy <lixiangmin01@baidu.com>
2019-09-22 12:41:16 +0800

    feature(chapter20): Implementing the Drop Trait on ThreadPool

--- chapter20/hello/src/lib.rs
+++ chapter20/hello/src/lib.rs
@@ -58,14 +58,16 @@ impl Drop for ThreadPool {
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
-            worker.thread.join().unwrap();
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
         }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -78,6 +80,9 @@ impl Worker {
             job.call_box();
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
