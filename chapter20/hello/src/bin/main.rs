b748e423db3bd789527c313a39916d3c3c8a32fe
Sammy <lixiangmin01@baidu.com>
2019-09-22 12:13:44 +0800

    feature(chapter20): Listing 20-13: Implementing ThreadPool::new to panic if size is zero

--- chapter20/hello/src/bin/main.rs
+++ chapter20/hello/src/bin/main.rs
@@ -5,6 +5,8 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
