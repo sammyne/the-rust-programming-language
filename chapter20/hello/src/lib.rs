0c09c35d25f24d6cfc7003f449331f635d06be4b
Sammy <lixiangmin01@baidu.com>
2019-09-22 12:46:00 +0800

    feature(chapter20): Listing 20-25: Sending Message::Terminate to the workers before calling join on each worker thread

--- chapter20/hello/src/lib.rs
+++ chapter20/hello/src/lib.rs
@@ -60,6 +60,14 @@ impl ThreadPool {
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
