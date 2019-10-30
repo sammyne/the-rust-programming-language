04e97a05aea3b3d8809e13ecf95e642079353bc5
Sammy <lixiangmin01@baidu.com>
2019-10-30 09:49:47 +0800

    feature(chapter06/listings): Listing 6-4: A Coin enum in which the Quarter variant also holds a UsState value

---
+++ chapter06/listings/_04/src/main.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    let c = Coin::Quarter(UsState::Alaska);
+
+    value_in_cents(c);
+}
