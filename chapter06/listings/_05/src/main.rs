3e42626dbea5db741200765ed1eb6503f856e7fb
Sammy <lixiangmin01@baidu.com>
2019-10-30 09:51:55 +0800

    feature(chapter06/listings): Listing 6-5: A function that uses a match expression on an Option<i32>

---
+++ chapter06/listings/_05/src/main.rs
@@ -0,0 +1,16 @@
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}", five);
+    println!("{:?}", six);
+    println!("{:?}", none);
+}
