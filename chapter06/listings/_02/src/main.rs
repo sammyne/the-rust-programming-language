d8f6222fcc13c0d399cfc95690a48238a54e4e7b
Sammy <lixiangmin01@baidu.com>
2019-10-30 09:41:06 +0800

    feature(chapter06/listings): Listing 6-2: A Message enum whose variants each store different amounts and types of values

---
+++ chapter06/listings/_02/src/main.rs
@@ -0,0 +1,26 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
+
+// alternative using struct
+// struct QuitMessage; // unit struct
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // tuple struct
+// struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+fn main() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
