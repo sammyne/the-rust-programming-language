9cc79235b4a4e2ce6f4c940eb9f4d2dd285accf2
Sammy <lixiangmin01@baidu.com>
2019-10-30 09:47:28 +0800

    feature(chapter06/listings): multiple lines of code in a match arm

--- chapter06/listings/_03/src/main.rs
+++ chapter06/listings/_03/src/main.rs
@@ -7,7 +7,10 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
@@ -15,5 +18,7 @@ fn value_in_cents(coin: Coin) -> u8 {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let c = Coin::Penny;
+
+    value_in_cents(c);
 }
