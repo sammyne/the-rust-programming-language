fbc45a019e6460b79eb4d9d618a2357fa4998751
sammyne <lixiangmin01@baidu.com>
2019-12-17 13:09:00 +0800

    feature(chapter15/listings): Listing 15-22: Using RefCell<T> to mutate an inner value while the outer value is considered immutable

--- chapter15/listings/_20_21_22/src/lib.rs
+++ chapter15/listings/_20_21_22/src/lib.rs
@@ -37,20 +37,21 @@ impl<'a, T> LimitTracker<'a, T>
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
-            MockMessenger { sent_messages: vec![] }
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
 
@@ -61,6 +62,6 @@ mod tests {
 
         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
