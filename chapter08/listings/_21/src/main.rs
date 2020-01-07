d0f21b02f786cbe8a4be1c4b256e5d08c7eca194
sammyne <lixiangmin01@baidu.com>
2020-01-07 09:39:58 +0800

    feature(chapter08/listings): Listing 8-21: Creating a hash map from a list of teams and a list of scores

---
+++ chapter08/listings/_21/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    use std::collections::HashMap;
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    println!("{:?}", scores);
+}
