f5eabac85a81f6f36859be43bb896ee6c0c3caae
sammyne <lixiangmin01@baidu.com>
2020-01-07 09:29:27 +0800

    feature(chapter08/listings): Listing 8-14: Storing greetings in different languages in strings

---
+++ chapter08/listings/_14/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let _hello = String::from("السلام عليكم");
+    let _hello = String::from("Dobrý den");
+    let _hello = String::from("_hello");
+    let _hello = String::from("שָׁלוֹם");
+    let _hello = String::from("नमस्ते");
+    let _hello = String::from("こんにちは");
+    let _hello = String::from("안녕하세요");
+    let _hello = String::from("你好");
+    let _hello = String::from("Olá");
+    let _hello = String::from("Здравствуйте");
+    let _hello = String::from("Hola");
+}
